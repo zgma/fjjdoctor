14f7a27dc14eeac6245812ad8ddb372254f76697
zgma <zgma@iflytek.com>
2020-11-23 11:30:58 +0800

    222

--- simplezxing/build/generated/source/buildConfig/debug/com/acker/simplezxing/BuildConfig.java
+++ simplezxing/build/generated/source/buildConfig/debug/com/acker/simplezxing/BuildConfig.java
@@ -5,9 +5,8 @@ package com.acker.simplezxing;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.acker.simplezxing";
+  public static final String LIBRARY_PACKAGE_NAME = "com.acker.simplezxing";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 6;
   public static final String VERSION_NAME = "1.6";
 }
